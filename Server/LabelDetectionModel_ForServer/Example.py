c742c5e0bab5faa88dd4bfecf47a08d82c920447
Minaminaminamima <qkralsdk39@naver.com>
2021-12-29 12:16:25 +0900

    add labelDetector for server

---
+++ Server/LabelDetectionModel_ForServer/Example.py
@@ -0,0 +1,7 @@
+import Labeldetection
+
+if __name__ == "__main__":
+
+    print(Labeldetection.LabelDetector("TEST1")) # yoserose
+    print(Labeldetection.LabelDetector("TEST2"))  # yellowtail
+    print(Labeldetection.LabelDetector("TEST3"))  # G7
