a352dfc233f9f5bda1915deb72aee75556c0e1cf
Kyoungjin <jinnymini@gmail.com>
2021-12-31 12:01:41 +0900

    server-edit model and add label recommendation

--- Server/tmwine/api/views.py
+++ Server/tmwine/api/views.py
@@ -1,3 +1,18 @@
+import os
+from pathlib import Path
+from PIL import Image
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+@api_view(['GET'])
+def labelRecommendation(request):
+    img = Image.open(request.FILES['label'])
+    imgName = request.data['label'].name
+    imgPath = os.path.join(os.path.join(BASE_DIR, 'label_img'), imgName)
+    img.save(imgPath, "JPEG")
+    return HttpResponse("Success")
